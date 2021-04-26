6c78fb5aec81c8c1c237c4335a43056300416318
ranjithamani <83059639+ranjithamani@users.noreply.github.com>
2021-04-26 21:49:03 +0530

    Add files via upload

---
+++ defaultvalue.java
@@ -0,0 +1,29 @@
+class defaultvalue
+ {
+byte b;
+short s;
+int i;
+long l;
+float f;
+double d;
+char c;
+boolean bu;
+public static void main(String[] args)
+
+{
+defaultvalue def = new defaultvalue();
+System.out.println(def.b);
+System.out.println(def.s);
+System.out.println(def.i);
+System.out.println(def.l);
+System.out.println(def.f);
+System.out.println(def.d);
+System.out.println(def.c);
+System.out.println(def.bu);
+
+}
+
+}
+
+
+
